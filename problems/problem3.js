08a47f69a187923f15e39e6957c1ac99875cbc1d
iftakhar <iftakharalamshuvo11@gmail.com>
2024-03-23 02:17:29 +0600

    problem 3 explanation

--- problems/problem3.js
+++ problems/problem3.js
@@ -20,3 +20,5 @@ return sum
 }
 
 console.log(sum(437));
+
+// prothome j nums passi seta to number e ase r jehetu number k for loop calano jai na tai takhe toString kore akta variable e rekhe dilam .and next akta sum variable nilam and ,abr string er upor for loop calalam and for loop every nums k access kore pele and sekhane prottekta element  k abr parseint kore karon parseint cara sum ber kora jabe na .so eta holo explanation .
